5c48a60aa89305eb45cb646b4014ad86d3cc1920
Timothy Vitkin <timothy.vitkin@gmail.com>
2023-02-25 18:33:33 -0600

    Begin work on custom markers

--- app/src/main/java/com/hackillinois/snapchatUIComposeClone/features/feature_snap_map/SnapMapScreen.kt
+++ app/src/main/java/com/hackillinois/snapchatUIComposeClone/features/feature_snap_map/SnapMapScreen.kt
@@ -1,5 +1,6 @@
 package com.hackillinois.snapchatUIComposeClone.features.feature_snap_map
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,16 +14,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.maps.android.compose.CameraPositionState
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.*
 import com.hackillinois.snapchatUIComposeClone.R
 import com.hackillinois.snapchatUIComposeClone.common.utils.ThemeColors
 import com.hackillinois.snapchatUIComposeClone.features.feature_snap_map.presentation.components.MapBottomNavigation
+import com.hackillinois.snapchatUIComposeClone.features.feature_snap_map.presentation.components.MapMarker
+
 
 @Composable
 @Preview
@@ -52,7 +53,20 @@ fun SnapMapScreen() {
                         LatLng(40.11395268359537, -88.22499888074972), 12f, 0f, 0f
                     )
                 )
-            )
+            ){
+                // make request to db
+                val realm = RealmProvider().getRealm().
+                // for every memory fetched, place marker
+
+                val markerPosition = LatLng(40.11395268359537, -88.22499888074972)
+                val markerState =
+                Marker(
+                    state = rememberMarkerState(position = markerPosition),
+                    title = "Marker1",
+                    snippet = "Marker in Singapore",
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
+                )
+            }
             Box(
                 modifier = Modifier
                     .rotate(180f)
---
+++ app/src/main/java/com/hackillinois/snapchatUIComposeClone/features/feature_snap_map/presentation/components/MapMarker.kt
@@ -0,0 +1,37 @@
+package com.hackillinois.snapchatUIComposeClone.features.feature_snap_map.presentation.components
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+
+fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+    return ContextCompat.getDrawable(context, vectorResId)?.run {
+        setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+        draw(Canvas(bitmap))
+        BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+}
+
+@Composable
+fun MapMarker(
+    context: Context,
+    position: LatLng,
+    title: String,
+    @DrawableRes iconResourceId: Int
+) {
+    val icon = bitmapDescriptorFromVector(
+        context, iconResourceId
+    )
+//    Marker(
+//        position = position,
+//
+//    )
+}
