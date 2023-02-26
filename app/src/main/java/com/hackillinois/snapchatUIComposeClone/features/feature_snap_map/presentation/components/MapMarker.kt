872c58be1ed75d445bbafbea8cea5835e588a85e
Timothy Vitkin <timothy.vitkin@gmail.com>
2023-02-26 00:57:59 -0600

    Marker finally looks like how i want it to

--- app/src/main/java/com/hackillinois/snapchatUIComposeClone/features/feature_snap_map/presentation/components/MapMarker.kt
+++ app/src/main/java/com/hackillinois/snapchatUIComposeClone/features/feature_snap_map/presentation/components/MapMarker.kt
@@ -6,14 +6,32 @@ import android.content.Context
 import android.graphics.*
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindow
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberMarkerState
 import com.hackillinois.snapchatUIComposeClone.R
 import com.hackillinois.snapchatUIComposeClone.common.models.Memory
@@ -31,17 +49,45 @@ fun MapMarker(
 //    val view = View.inflate(context, R.layout.image_layout, null)
 
     val markerPosition = LatLng(memory.latitude, memory.longitude)
-    Marker(
-        state = rememberMarkerState(position = markerPosition),
-        title = if(memory.name.isEmpty()) "Memory" else memory.name,
-        snippet = "Unlocks at ${memory.unlockedAt}",
-//        anchor = Offset(0.5f, 1f),
-        icon = BitmapDescriptorFactory.defaultMarker(),
-        onClick = {
-            Log.d("MapMarker", "Clicked on marker")
-            return@Marker true
+//    Marker(
+//        state = rememberMarkerState(position = markerPosition),
+//        title = "Marker1",
+//        snippet = "Marker in Singapore",
+//        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
+//    )
+
+    MarkerInfoWindow(
+        state = MarkerState(position = markerPosition),
+        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE),
+    ) { marker ->
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    shape = RoundedCornerShape(35.dp, 35.dp, 35.dp, 35.dp),
+
+                )
+            ,
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.blanc),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(35.dp, 35.dp, 35.dp, 35.dp))
+                )
+
         }
-    )
+
+
+    }
+
+
+
+
 
 
 }
